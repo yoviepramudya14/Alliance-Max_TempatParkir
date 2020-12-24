62b956bfef14a2950b9f0d7ffc38cd2865d5acf6
Ulfia Khairani <72622275+Ulfiakhairani@users.noreply.github.com>
2020-12-24 14:13:26 +0700

    Perubahan pada class

--- src/ParkingsManagements/Konfigurasi.java
+++ src/ParkingsManagements/Konfigurasi.java
@@ -1,33 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ParkingsManagements;
 
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Konfigurasi {
-    
     private static Connection MySQLconfig;
     
-    public static Connection configDB()throws SQLException{
+    public static Connection configDB()throws java.sql.SQLException{
+
         try{
-            String url = "jdbc:mysql://127.0.0.1:3306/parkiran?serverTimezone=UTC";
-            String user = "root";
-            String pass = "";
+            String url = "jdbc:oracle:thin:@192.168.56.1:1521:XE";
+            String user = "ulfia";
+            String pass = "1234";
             
-            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            //String driverName = "oracle.jdbc.driver.OracleDriver";
+            //Class.forName(driverName);
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
             MySQLconfig = DriverManager.getConnection(url, user, pass);
             System.out.println("Koneksi ke Database berhasil");
-            //MySQLconfig = DriverManager.getConnection("jdbc:oracle:thin:@192.168.56.1:1521:XE","ulfia","dataparkit");
-        }catch(SQLException e){
+        }catch(java.sql.SQLException e){
             System.out.println("Koneksi ke Database gagal " + e.getMessage());
         }
-        
-        return MySQLconfig;
+    return MySQLconfig;
     }
-    
 }
